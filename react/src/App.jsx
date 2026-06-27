8c0064eaa5304b7fa5409487ad03f6474bd49635
“Tejas9420190282” <tejasshimpi877@gmail.com>
2026-06-27 12:03:03 +0530

    Added protected routes to Home and MeetingRoom page

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -6,30 +6,51 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import MeetingRoom from "./pages/MeetingRoom";
 import Register from "./pages/Register";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Login />
-    },
-    {
-        path: "/register",
-        element: <Register />
-    },
-    {
-        path: "/home",
-        element: <Home />
-    },
-    {
-        path: "/meeting/:roomId",
-        element: <MeetingRoom   />
-    },
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/home",
+    element: (
+      <ProtectedRoute>
+        <Home />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/meeting/:roomId",
+    element: (
+      <ProtectedRoute>
+        <MeetingRoom />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/profile",
+    element: (
+      <ProtectedRoute>
+        <>
+          <div>
+            This feature Is not Available at this time. should be added soon
+          </div>
+        </>
+      </ProtectedRoute>
+    ),
+  },
 ]);
 
 function App() {
   return (
     <>
-        <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </>
   );
 }
